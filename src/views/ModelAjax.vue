00397faafdb00ac6480950e86af441c4a54e39fa
yogeshhelpful <007saurabhgoyal@gmail.com>
2020-11-05 00:28:53 +0530

    chngae

--- src/views/ModelAjax.vue
+++ src/views/ModelAjax.vue
@@ -37,44 +37,6 @@
         </div>
       </div>
     </div>
-    <div class="ui vertical segment">
-      <div class="flexbox">
-        <div class="flex-content">
-          <h3>Init with ajax</h3>
-          <div>
-            <model-list-select
-              :list="animations"
-              option-value="id"
-              :custom-text="optionDisplayText"
-              v-model="selectedAnimation"
-              placeholder="select item"
-              @searchchange="printSearchText"
-            >
-            </model-list-select>
-          </div>
-        </div>
-        <div class="flex-result">
-          <h4>input text(searchText)</h4>
-          <p>{{searchText2}}</p>
-          <table class="ui celled table">
-            <thead>
-            <tr>
-              <th>id</th>
-              <th>title_short1</th>
-              <th>twitter_account</th>
-              <th>public_url</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <td>{{selectedAnimation.id}}</td>
-              <td>{{selectedAnimation.title_short1}}</td>
-              <td>{{selectedAnimation.twitter_account}}</td>
-              <td>{{selectedAnimation.public_url}}</td>
-            </tr>
-            </tbody>
-          </table>
-        </div>
       </div>
     </div>
   </div>
@@ -89,15 +51,8 @@ export default {
   data () {
     return {
       countries: [],
-      selectedCountry: {},
-      searchText: '',
-      animations: [],
-      selectedAnimation: {},
-      searchText2: ''
+      selectedCountry: {}
     }
-  },
-  created () {
-    this.searchAnimation()
   },
   methods: {
     searchCountry (searchText) {
@@ -105,27 +60,6 @@ export default {
       ajaxFindCountry(searchText).then(response => {
         this.countries = response
       })
-    },
-    printSearchText (searchText) {
-      this.searchText2 = searchText
-    },
-    /**
-     * api : https://qiita.com/AKB428/items/64938febfd4dcf6ea698
-     */
-    searchAnimation () {
-      if (this.animations.length === 0) {
-        axios.get(`http://api.moemoe.tokyo/anime/v1/master/2017/2`)
-          .then(response => {
-            this.animations = response.data
-          })
-          .catch(error => {
-            /* eslint no-console: off */
-            console.error(error)
-          })
-      }
-    },
-    optionDisplayText (animation) {
-      return `${animation.title_short1} - ${animation.twitter_account} - ${animation.public_url}`
     }
   },
   components: {
